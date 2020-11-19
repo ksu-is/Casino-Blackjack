c74bd2537e5f2b5e8123d8090661ac1b4b3d3599
andrewhess3 <ahessefo@students.kennesaw.edu>
2020-11-19 18:25:27 -0500

    Created card class
    Created the class for the card with suit, rank, and score.

--- process.py
+++ process.py
@@ -4,3 +4,11 @@ import random
 class card:
     def __init__(self,suit,rank):
         self.suit = suit
+        self.rank = rank
+        if rank == "K" or rank == "Q" or rank =="J":
+            self.score = 10
+        elif rank == "A":
+            self.score = 1
+        else:
+            self.score = int(rank)
+    print(self.suit + self.rank)
